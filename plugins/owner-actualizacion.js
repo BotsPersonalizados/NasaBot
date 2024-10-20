ff009adb05bc9b052a55266fa7edbf48d7d1e5cc
BotsPersonalizados <lxspisco@gmail.com>
2024-10-19 19:33:40 -0500

    Update owner-actualizacion.js

--- plugins/owner-actualizacion.js
+++ plugins/owner-actualizacion.js
@@ -4,7 +4,7 @@ let previousCommitSHA = '';
 let previousUpdatedAt = '';
 let previousCommitUser = ''; 
 const owner = '51920011639';
-const repo = 'TheMystic-Bot-MD';
+const repo = 'NasaBot';
 const handler = async (m, {conn, text, usedPrefix, command}) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
